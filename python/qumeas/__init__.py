1d9c8601849fd80034fb011c22512b64a2fb2db5
oimeitei <life_oinam@yahoo.com>
2024-11-09 19:10:38 -0500

    doc: fix documentation - fixed version issue in documentation

--- python/qumeas/__init__.py
+++ python/qumeas/__init__.py
@@ -1,5 +1,8 @@
-from ._version import __version__
 from .cumulant import QCumulant
 from .pauli_container import PauliContainer
 from .random_measurement import RandomShadow
 from .state_prep import StatePreparation
+try:
+   from ._version import __version__
+except:
+   __version__ = 'development version'
